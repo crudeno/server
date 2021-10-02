9e19dcf538931b8d5d0100cee71a735d370fcd05
Davide Caruso <davide.caruso93@gmail.com>
2021-10-02 17:06:31 +0200

    Use type instead of interface for Config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
-export default interface Config {
+type Config = {
   readonly host?: string;
   readonly port?: number;
   readonly path?: string;
   readonly type?: 'graphql' | 'rest';
 }
+
+export default Config
